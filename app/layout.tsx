78371c24a795d6cba79f90dc221273bb38a89028
ShariyarAbeer <007abeershariyar@gmail.com>
2025-09-14 01:38:43 +0600

    customize redix ui theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,21 @@
 import "@radix-ui/themes/styles.css";
+import "./radix-themes.css"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./NavBar";
-import { Theme } from "@radix-ui/themes";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+ 
 });
 
 export const metadata: Metadata = {
@@ -26,16 +29,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={geistSans.variable}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Theme>
+        <Theme accentColor="red" radius="large">
           
         <NavBar/>
        <main className="p-5">
          {children}
        </main>
+      
         </Theme>
       </body>
     </html>
