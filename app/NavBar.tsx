daa10ee5e9e160738c54ea51b517242f80836908
ShariyarAbeer <007abeershariyar@gmail.com>
2025-09-13 16:13:23 +0600

    style active links in the nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,13 @@
+"use client"
 import Link from 'next/link'
 import React from 'react'
 import { FaBug } from "react-icons/fa";
+import { usePathname } from 'next/navigation';
+import  classnames  from "classnames";
+import classNames from 'classnames';
 
 const NavBar = () => {
+  const pathName = usePathname();
   const links = [
     {label: "Dashboard", href:"/"},
     {label: "Issues", href:"/issues"},
@@ -12,8 +17,17 @@ const NavBar = () => {
     <Link href={"/"}><FaBug /></Link>
     <ul className='flex space-x-6 '>
     
-        {links.map(link => <Link key={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors' href={link.href}>{link.label}</Link>)}
+        {/* {links.map(link => <Link key={link.href} className={`${link.href === pathName ? 'text-zinc-900' : 'text-zinc-500'} hover:text-zinc-800 transition-colors`}  */}
+        {links.map(link => <Link key={link.href} 
+        className={classnames({
+          "text-zinc-900" : link.href === pathName,
+          "text-zinc-500" : link.href !== pathName,
+          "hover:text-zinc-800 transition-colors" : true,
+        })}
+        
+        href={link.href}>{link.label}</Link>)}
     </ul>
+    <h1>{pathName}</h1>
 </nav>
   )
 }
